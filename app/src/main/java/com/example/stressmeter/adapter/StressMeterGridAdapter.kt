725af398f5e8e6af7f552c705d9bc3ce6e2602cd
Deniz Eren Evrendilek <devrendilek@gmail.com>
2023-10-15 18:14:20 -0700

    Add scores for images

--- app/src/main/java/com/example/stressmeter/adapter/StressMeterGridAdapter.kt
+++ app/src/main/java/com/example/stressmeter/adapter/StressMeterGridAdapter.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AbsListView
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.TextView
 import com.example.stressmeter.R
+import com.example.stressmeter.ui.home.StressMeterViewModel
 
 class StressMeterGridAdapter(private val context: Context, private val imageUrls: List<Int>) :
         ArrayAdapter<Int>(context, 0, imageUrls) {
@@ -16,8 +17,16 @@ class StressMeterGridAdapter(private val context: Context, private val imageUrls
             convertView ?: LayoutInflater.from(context)
                 .inflate(R.layout.grid_item_stress_meter, parent, false)
         val imageView: ImageView = view.findViewById(R.id.image_view_item)
-        imageView.setImageResource(imageUrls[position])
-        imageView.tag = imageUrls[position]
+        val textView: TextView = view.findViewById(R.id.image_score)
+
+        val imageResourceId = imageUrls[position]
+        val imageStressScore = StressMeterViewModel.getStressScore(imageResourceId).toString()
+        val desc = "Score: $imageStressScore"
+        println(desc)
+        imageView.setImageResource(imageResourceId)
+        imageView.tag = imageResourceId
+        textView.text = desc
+        textView.background.alpha = 175
         return view
     }
 }
--- app/src/main/java/com/example/stressmeter/ui/home/StressMeterViewModel.kt
+++ app/src/main/java/com/example/stressmeter/ui/home/StressMeterViewModel.kt
@@ -5,6 +5,58 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.stressmeter.R
 
+private val imageToStressLevelMap = mapOf(
+    R.drawable.psm_alarm_clock to 5,
+    R.drawable.fish_normal017 to 3,
+    R.drawable.psm_alarm_clock2 to 6,
+    R.drawable.psm_angry_face to 10,
+    R.drawable.psm_anxious to 7,
+    R.drawable.psm_baby_sleeping to 0,
+    R.drawable.psm_exam4 to 9,
+    R.drawable.psm_gambling4 to 8,
+    R.drawable.psm_bar to 4,
+    R.drawable.psm_barbed_wire2 to 9,
+    R.drawable.psm_beach3 to 0,
+    R.drawable.psm_bird3 to 1,
+    R.drawable.psm_blue_drop to 2,
+    R.drawable.psm_cat to 3,
+    R.drawable.psm_clutter to 8,
+    R.drawable.psm_clutter3 to 7,
+    R.drawable.psm_dog_sleeping to 0,
+    R.drawable.psm_headache to 7,
+    R.drawable.psm_headache2 to 8,
+    R.drawable.psm_hiking3 to 4,
+    R.drawable.psm_kettle to 3,
+    R.drawable.psm_lake3 to 1,
+    R.drawable.psm_lawn_chairs3 to 3,
+    R.drawable.psm_lonely to 8,
+    R.drawable.psm_lonely2 to 7,
+    R.drawable.psm_mountains11 to 4,
+    R.drawable.psm_neutral_child to 1,
+    R.drawable.psm_neutral_person2 to 1,
+    R.drawable.psm_peaceful_person to 1,
+    R.drawable.psm_puppy to 0,
+    R.drawable.psm_puppy3 to 1,
+    R.drawable.psm_reading_in_bed2 to 2,
+    R.drawable.psm_running3 to 5,
+    R.drawable.psm_running4 to 6,
+    R.drawable.psm_sticky_notes2 to 3,
+    R.drawable.psm_stressed_cat to 5,
+    R.drawable.psm_stressed_person to 10,
+    R.drawable.psm_stressed_person12 to 9,
+    R.drawable.psm_stressed_person3 to 7,
+    R.drawable.psm_stressed_person4 to 8,
+    R.drawable.psm_stressed_person6 to 7,
+    R.drawable.psm_stressed_person7 to 8,
+    R.drawable.psm_stressed_person8 to 9,
+    R.drawable.psm_talking_on_phone2 to 5,
+    R.drawable.psm_to_do_list to 3,
+    R.drawable.psm_to_do_list3 to 4,
+    R.drawable.psm_wine3 to 6,
+    R.drawable.psm_work4 to 6,
+    R.drawable.psm_yoga4 to 2
+)
+
 class StressMeterViewModel : ViewModel() {
     private val _imageIds = listOf(
         R.drawable.psm_alarm_clock,
@@ -62,6 +114,7 @@ class StressMeterViewModel : ViewModel() {
 
     private var _activeImageIds = MutableLiveData<List<Int>>()
     private var _selectedImageId = MutableLiveData<Int>()
+    private val _imageKeys = imageToStressLevelMap.keys
 
     val selectedImageMutable: LiveData<Int>
         get() = _selectedImageId
@@ -73,11 +126,10 @@ class StressMeterViewModel : ViewModel() {
         _selectedImageId.value = id
     }
 
-
     fun next(iterateIndex: Boolean = true) {
         // get next 16 images
-        val curr = _imageIds
-        val size = _imageIds.size
+        val curr = _imageKeys.toList()
+        val size = curr.size
 
         if (iterateIndex)
             index = (index + 16) % size
@@ -93,5 +145,14 @@ class StressMeterViewModel : ViewModel() {
             curr.subList(index, size) + curr.subList(0, nextIndex)
         }
         return items
+    }
+
+    companion object {
+        fun getStressScore(id: Int): Int {
+            if (imageToStressLevelMap[id] == null) {
+                throw IllegalAccessError("Image id not found")
+            }
+            return imageToStressLevelMap[id]!!
+        }
     }
 }
